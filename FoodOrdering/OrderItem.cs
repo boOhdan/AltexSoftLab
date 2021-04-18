99e4f78c16c7108a8896db2699193bb79739839c
XIAOIIII <boOhdan>
2021-04-18 13:02:46 +0300

    created models for food ordering system

---
+++ FoodOrdering/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrdering
+{
+    class OrderItem
+    {
+        public Meal Meal { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ FoodOrdering/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrdering
+{
+    class Restaurant
+    {
+        public string Name { get; set; }
+        public ICollection<Meal> Menu { get; set; }
+        public string Address { get; set; }
+    }
+}
