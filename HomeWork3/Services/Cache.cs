ee6d887b5a3d346a7ec22099a848f7109051ec60
XIAOIIII <boOhdan>
2021-06-10 14:29:11 +0300

    created Caching

---
+++ HomeWork3/Services/Cache.cs
@@ -0,0 +1,45 @@
+﻿using FoodOrdering.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoodOrdering.Services
+{
+    public class Cache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        private readonly object _cacheLock = new object();
+        private readonly int _sizeLimit;
+        private Dictionary<TKey, TValue> _cache = new Dictionary<TKey, TValue>();
+
+        public Cache(int sizeLimit = 5) 
+        {
+            _sizeLimit = sizeLimit > 0 ? sizeLimit : throw new ArgumentException();
+        }
+
+        public TValue GetOrCreate(TKey key, Func<TKey, TValue> createElement)
+        {
+            lock(_cacheLock) 
+            {
+                if (!_cache.ContainsKey(key))
+                {
+                    if (IsCacheFull())
+                        DeleteLastElement();
+
+                    _cache[key] = createElement(key);
+                }
+            }
+  
+            return _cache[key];
+        }
+
+        public bool IsCacheFull() 
+        {
+            return _cache.Count == _sizeLimit;
+        }
+
+        public void DeleteLastElement() 
+        {
+            _cache.Remove(_cache.Keys.Last());
+        }
+    }
+}
