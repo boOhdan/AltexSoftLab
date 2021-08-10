8988a60781db9f9822e86eb25acad517344e6ba1
XIAOIIII <boOhdan>
2021-08-10 10:22:16 +0300

    created User model, derived Courier and Customer from User

--- EntityFramework/HomeWork1/Models/Courier.cs
+++ EntityFramework/HomeWork1/Models/Courier.cs
@@ -2,10 +2,5 @@
 {
     public class Courier
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
--- EntityFramework/HomeWork1/Models/Customer.cs
+++ EntityFramework/HomeWork1/Models/Customer.cs
@@ -1,11 +1,6 @@
 ﻿namespace FoodOrderingSystem.Models
 {
-    public class Customer
+    public class Customer : User
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
