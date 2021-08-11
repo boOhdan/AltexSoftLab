af90aabd841782864d58b2906dee6186d7d275e6
XIAOIIII <boOhdan>
2021-08-12 01:03:34 +0300

    created homework №4

---
+++ EntityFramework/HomeWork4/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace FoodOrderingSystem.Models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
