d774fa226159789d673861e22333196b4e420d19
XIAOIIII <boOhdan>
2021-08-26 09:36:33 +0300

    Added explicit acess modifiers to class and method

--- EntityFramework/HomeWork3/Program.cs
+++ EntityFramework/HomeWork3/Program.cs
@@ -7,9 +7,9 @@ using System.Text;
 
 namespace FoodOrderingSystem
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
 
